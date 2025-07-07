c42a7ca321d07efe556999c77183f46f4c971e2e
Merdi madimba🍀 <161202374+Zorolefrerot@users.noreply.github.com>
2025-07-07 20:04:22 +0100

    Update city-data.js

--- scripts/cmds/city-data.js
+++ scripts/cmds/city-data.js
@@ -1,5 +1,224 @@
-// Se met à jour automatiquement : NE PAS modifier manuellement !
+
 module.exports = {
-  cities: {},
-  market: { prices: {}, lastUpdate: 0 }
-};
+  "cities": {
+    "61570839220831": {
+      "name": "Emirats Arabes Unis",
+      "gold": 1624,
+      "pop": 50,
+      "lvl": 1,
+      "army": 7,
+      "b": {
+        "house": 1,
+        "farm": 1,
+        "mine": 0,
+        "factory": 0,
+        "bank": 7,
+        "school": 0,
+        "transport": 0,
+        "barracks": 5
+      },
+      "res": {
+        "wood": 0,
+        "stone": 9,
+        "iron": 0,
+        "copper": 11,
+        "coal": 0,
+        "goldOre": 11
+      },
+      "produce": [
+        "copper",
+        "goldOre",
+        "stone"
+      ],
+      "notif": [],
+      "lastCollect": 1751908558277,
+      "lastAttack": 1751908270907
+    },
+    "61576596194735": {
+      "name": "INCHEON",
+      "gold": 314,
+      "pop": 100,
+      "lvl": 1,
+      "army": 0,
+      "b": {
+        "house": 11,
+        "farm": 1,
+        "mine": 0,
+        "factory": 0,
+        "bank": 3,
+        "school": 0,
+        "transport": 0,
+        "barracks": 0
+      },
+      "res": {
+        "wood": 58,
+        "stone": 37,
+        "iron": 55,
+        "copper": 31,
+        "coal": 58,
+        "goldOre": 72
+      },
+      "produce": [
+        "wood",
+        "coal",
+        "goldOre"
+      ],
+      "notif": [],
+      "lastCollect": 1751914894700,
+      "lastAttack": 0
+    },
+    "100056763252164": {
+      "name": "Shibuya",
+      "gold": 884,
+      "pop": 50,
+      "lvl": 1,
+      "army": 4,
+      "b": {
+        "house": 1,
+        "farm": 1,
+        "mine": 0,
+        "factory": 0,
+        "bank": 2,
+        "school": 0,
+        "transport": 0,
+        "barracks": 4
+      },
+      "res": {
+        "wood": 24,
+        "stone": 20,
+        "iron": 34,
+        "copper": 24,
+        "coal": 30,
+        "goldOre": 20
+      },
+      "produce": [
+        "wood",
+        "iron",
+        "copper"
+      ],
+      "notif": [],
+      "lastCollect": 1751907987954,
+      "lastAttack": 0
+    },
+    "100081383771175": {
+      "name": "wassoulou",
+      "gold": 787,
+      "pop": 50,
+      "lvl": 1,
+      "army": 0,
+      "b": {
+        "house": 1,
+        "farm": 1,
+        "mine": 0,
+        "factory": 0,
+        "bank": 3,
+        "school": 0,
+        "transport": 0,
+        "barracks": 0
+      },
+      "res": {
+        "wood": 32,
+        "stone": 41,
+        "iron": 30,
+        "copper": 48,
+        "coal": 44,
+        "goldOre": 23
+      },
+      "produce": [
+        "coal",
+        "copper",
+        "stone"
+      ],
+      "notif": [
+        "⚔️ Dubaï 𝗮 𝘃𝗼𝗹𝗲́ 138$."
+      ],
+      "lastCollect": 1751910116370,
+      "lastAttack": 0
+    },
+    "100086576258820": {
+      "name": "giuytebeibei",
+      "gold": 321,
+      "pop": 75,
+      "lvl": 1,
+      "army": 0,
+      "b": {
+        "house": 6,
+        "farm": 1,
+        "mine": 1,
+        "factory": 0,
+        "bank": 11,
+        "school": 1,
+        "transport": 0,
+        "barracks": 1
+      },
+      "res": {
+        "wood": 0,
+        "stone": 37,
+        "iron": 0,
+        "copper": 36,
+        "coal": 39,
+        "goldOre": 1
+      },
+      "produce": [
+        "coal",
+        "stone",
+        "copper"
+      ],
+      "notif": [
+        "⚔️ Dubaï 𝗮 𝘃𝗼𝗹𝗲́ 126$.",
+        "⚔️ Dubaï 𝗮 𝘃𝗼𝗹𝗲́ 108$.",
+        "⚔️ Dubaï 𝗮 𝘃𝗼𝗹𝗲́ 91$.",
+        "⚔️ Dubaï 𝗮 𝘃𝗼𝗹𝗲́ 78$.",
+        "⚔️ Dubaï 𝗮 𝘃𝗼𝗹𝗲́ 66$.",
+        "⚔️ Dubaï 𝗮 𝘃𝗼𝗹𝗲́ 56$."
+      ],
+      "lastCollect": 1751913885351,
+      "lastAttack": 1751911938665
+    },
+    "61552538030584": {
+      "name": "Dubaï",
+      "gold": 7928,
+      "pop": 20,
+      "lvl": 1,
+      "army": 18,
+      "b": {
+        "house": 3,
+        "farm": 3,
+        "mine": 3,
+        "factory": 3,
+        "bank": 32,
+        "school": 3,
+        "transport": 3,
+        "barracks": 11
+      },
+      "res": {
+        "wood": 38,
+        "stone": 0,
+        "iron": 30,
+        "copper": 24,
+        "coal": 0,
+        "goldOre": 19
+      },
+      "produce": [
+        "copper",
+        "iron",
+        "goldOre"
+      ],
+      "notif": [],
+      "lastCollect": 1751914940616,
+      "lastAttack": 1751914677843
+    }
+  },
+  "market": {
+    "prices": {
+      "wood": 5,
+      "stone": 10,
+      "iron": 18,
+      "copper": 20,
+      "coal": 10,
+      "goldOre": 37
+    },
+    "lastUpdate": 1751904986422
+  },
+  "location": "/opt/render/project/src/scripts/cmds/city-data.js"
+  }
